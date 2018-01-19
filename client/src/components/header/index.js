ff68195bdfd7d69aa14db2607ae9625e0d235ea1
Raegan Millhollin <desdemonhu@gmail.com>
2018-01-19 13:43:40 -0800

    Deleted unnecessary link in nav

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -10,7 +10,6 @@ export default class Header extends Component {
 				<nav>
 					<Link activeClassName={style.active} href="/">Home</Link>
 					<Link activeClassName={style.active} href="/profile">Me</Link>
-					<Link activeClassName={style.active} href="/profile/john">John</Link>
 				</nav>
 			</header>
 		);
