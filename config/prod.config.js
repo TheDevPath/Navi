d31a4b88c6b7599c92bce4b07e3b0107d6cde46b
John Kwening <kwening.john@gmail.com>
2018-01-20 23:45:58 -0500

    Setup MongoDB connection (#28)
    * Setup MongoDB connection
    
    Add database.js to config/ with url to database connection port.
    
    Setup connection to local MongoDB in app.js
    
    * Move database url out of config/database.js
    
    Delete config/database.js and move database.url into dev.config.js and
    prod.config.js to avoid having multiple configuration files and use
    existing configuration files established for dev/prod envs.

--- config/prod.config.js
+++ config/prod.config.js
@@ -2,4 +2,6 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'production',
   HOST: process.env.HOST,
   PORT: process.env.PORT,
+  // TODO - replace 'your_secret' with shorthand app name once that is decided
+  DB_URL : 'mongodb://localhost/your_secret'  // using default port (27017)
 };
