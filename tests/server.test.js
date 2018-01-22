7d571085052e13b75395282c84817adaedafa619
Evan <CodeDraken@users.noreply.github.com>
2018-01-22 17:40:53 -0600

    Setup mocha test suite for the server (#44)
    * docs: Add Contributing Documentation (#22) (#23)
    
    * docs: Add Contributing Documentation
    
    Documentation outlining how to contribute to the project.
    
    Resolves: #5
    
    * Fix minor typos
    
    * Setup mocha test suite for the server
    
    * change Mocha/Expect (jest) to Mocha/Chai with expect style as default
    
    * Remove yarn lock because main project does not use Yarn

---
+++ tests/server.test.js
@@ -0,0 +1,9 @@
+it('test runner should work', () => {
+  const foo = 'bar';
+  const beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+
+  expect(foo).to.be.a('string');
+  expect(foo).to.equal('bar');
+  expect(foo).to.have.lengthOf(3);
+  expect(beverages).to.have.property('tea').with.lengthOf(3);
+});
