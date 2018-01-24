6121565026bba3b4229a327466a0ce54a5f9dcc4
Bryan <motosharpley@gmail.com>
2018-01-24 12:19:52 -0600

    starter app w test suites added 1.1.2 (#55)
    * Corrected color theme.  (#42)
    
    * Add color theme images and text file.
    
    * Add text desc. and links to color values
    
    * Updating color theme to correct links.
    
    * Add table and color values. Remove broken link.
    
    * Add initial db models/schema (#45)
    
    Add saved-directions, saved-pins, and search-history models for app.
    Additionally merged current development branch to brach.
    
    * Setup mocha test suite for the server (#44)
    
    * docs: Add Contributing Documentation (#22) (#23)
    
    * docs: Add Contributing Documentation
    
    Documentation outlining how to contribute to the project.
    
    Resolves: #5
    
    * Fix minor typos
    
    * Setup mocha test suite for the server
    
    * change Mocha/Expect (jest) to Mocha/Chai with expect style as default
    
    * Remove yarn lock because main project does not use Yarn
    
    * Update PULL_REQUEST_TEMPLATE.md (#49)
    
    Added "Descriptive Branch Naming" to the Pull Request Template as well as added a "Thank You for Contributing" comment.
    
    * Development server runs on port 8081 (#54)
    
    * Issue 47 preact testing (#52)
    
    * Set up testing, but doesn't do shallow render yet
    
    * Testing components works!
    
    * sdfsdf
    
    * Added client test script to server test script
    
    * Added cross-env and updated test script, now both tests run for me

--- config/dev.config.js
+++ config/dev.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   HOST: process.env.HOST || 'http://localhost',
-  PORT: process.env.PORT || 3001,
+  PORT: process.env.PORT || 8081,
   DB_URL : 'mongodb://localhost/app_db'  // using default port (27017)
 };
