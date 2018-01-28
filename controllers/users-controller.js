d012473119e460cc372259c1292918bf631dde1c
Bryan <motosharpley@gmail.com>
2018-01-28 13:45:32 -0600

    Update users-controller.js

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,4 +1,4 @@
-const SeachHistory = require('../models/users');
+const SearchHistory = require('../models/users');
 
 /**
  * TODO - export functions to be used for completing requests in ../routes/users.js
