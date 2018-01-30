4bb1b78efa0cfb1151420586d6c167148b264e86
John Kwening <kwening.john@gmail.com>
2018-01-30 00:55:49 -0500

    refactor: Use require to parse secrets.json
    Nodejs 'require' will automatically read and parse content of a
    particular JSON object and assign the same to the variable placed on the
    left hand side.

--- config/dev.config.js
+++ config/dev.config.js
@@ -1,12 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-
-const JSON_SECRETS = JSON.parse(fs.readFileSync(path.join(__dirname, 'secrets.json')));
+const SECRETS = require('./secrets.json');  // nodejs will auto read json
 
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   HOST: process.env.HOST || 'http://localhost',
   PORT: process.env.PORT || 8081,
-  DB_URL : JSON_SECRETS.mongodb_dev.db_url,
-  GOOGLE_API_KEY: JSON_SECRETS.google_maps.api_key
+  DB_URL : SECRETS.mongodb_dev.db_url,
+  GOOGLE_API_KEY: SECRETS.google_maps.api_key
 };
--- config/prod.config.js
+++ config/prod.config.js
@@ -1,12 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-
-const JSON_SECRETS = JSON.parse(fs.readFileSync(path.join(__dirname, 'secrets.json')));
+const SECRETS = require('./secrets.json');
 
 module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'production',
   HOST: process.env.HOST,
   PORT: process.env.PORT,
-  DB_URL : JSON_SECRETS.mongodb_prod.db_url,
-  GOOGLE_API_KEY: JSON_SECRETS.google_maps.api_key
+  DB_URL : SECRETS.mongodb_prod.db_url,
+  GOOGLE_API_KEY: SECRETS.google_maps.api_key
 };
