6a0a35817e28b1bbdbffd5b41139a8105886c997
John Kwening <kwening.john@gmail.com>
2018-01-30 01:03:31 -0500

    feat: Add /maps/geolocation route (#62)
    * feat: Add /maps/geolocation route
    
    GET call to /maps/geoloction server returns geolocation of the app at
    for its present location as JSON
    
    Add secrets-example.json to ./config/ to represent secrets.json file
    used to store keys and private urls for security purposes. Refactor
    dev.config.js and prod.config.js to use values in secrets.json to
    populate DB_URL and GOOGLE_API_KEY for security reasons.
    
    * refactor: Use require to parse secrets.json
    
    Nodejs 'require' will automatically read and parse content of a
    particular JSON object and assign the same to the variable placed on the
    left hand side.

--- controllers/google-api-controller.js
+++ controllers/google-api-controller.js
@@ -1,4 +1,42 @@
+const http = require('https');
+const {GOOGLE_API_KEY} = require('../config');
+
 /**
- * TODO - configure connection to api and then define functions needed to
- * generate map, pins, directions, and search query results
+ * Geolocation API access
+ * Geolocation requests are sent using POST to the following URL:
+ * https://www.googleapis.com/geolocation/v1/geolocate?key=<YOUR_KEY>
  */
+const GEOLOCATION_OPTIONS = {
+  "method": "POST",
+  "hostname": "www.googleapis.com",
+  "port": null,
+  "path": `/geolocation/v1/geolocate?key=${GOOGLE_API_KEY}`,
+  "headers": {
+    "content-type": "application/json",
+    "cache-control": "no-cache"
+  }
+};
+
+/**
+ * @description Hanlder for Geolocation API call
+ * @returns {JSON} 'location': estimated 'lat' and 'lng'; 'accuracy': accuracy of
+ * the estimated location, in meters
+ */
+exports.getGeolocation = function(appReq, appRes) {
+    const req = http.request(GEOLOCATION_OPTIONS, function (res) {
+        const chunks = [];
+      
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+      
+        res.on("end", function () {
+          const body = Buffer.concat(chunks);
+          console.log(body.toString());
+          appRes.send(JSON.parse(body.toString()));
+        });
+      });
+      
+      req.write(JSON.stringify({}));
+      req.end();
+}
