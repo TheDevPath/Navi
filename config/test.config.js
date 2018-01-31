dd96cd0565d0b16cfb47d165bec1959f1a50433a
mallek <travis@haleycomputersolutions.com>
2018-01-31 02:03:54 -0700

    Add saved pins GET and POST with Tests.  Set up test environment

---
+++ config/test.config.js
@@ -0,0 +1,9 @@
+const SECRETS = require('./secrets.json'); // nodejs will auto read json
+
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  HOST: process.env.HOST || 'http://localhost',
+  PORT: process.env.PORT || 8081,
+  DB_URL: `${SECRETS.mongodb_dev.db_url}test`,
+  GOOGLE_API_KEY: SECRETS.google_maps.api_key,
+};
--- controllers/saved-pins-controller.js
+++ controllers/saved-pins-controller.js
@@ -3,3 +3,26 @@ const SavedPins = require('../models/saved-pins');
 /**
  * TODO - export functions to be used for completing requests in ../routes/search.js
  */
+
+exports.getSavedPins = (appReq, appRes) => {
+  SavedPins.find().then((savedPins) => {
+    appRes.send({ savedPins });
+  }, (e) => {
+    appRes.status(400).send(e);
+  });
+};
+
+exports.postSavedPins = (appReq, appRes) => {
+  const savedPin = new SavedPins({
+    lat: appReq.body.lat,
+    lng: appReq.body.lng,
+    place_id: appReq.body.place_id,
+  });
+
+  savedPin.save().then((pin) => {
+    appRes.send(pin);
+  }, (e) => {
+    appRes.status(400).send(e);
+  });
+};
+
