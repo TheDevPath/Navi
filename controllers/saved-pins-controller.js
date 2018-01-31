14116bb874be1c4c6ca56f2a3de7183345d9c5aa
mallek <travis@haleycomputersolutions.com>
2018-01-31 13:22:35 -0700

    Add Docstring for SavedPins API calls controller.  Change TestDB Secrets and example

--- controllers/saved-pins-controller.js
+++ controllers/saved-pins-controller.js
@@ -4,6 +4,15 @@ const SavedPins = require('../models/saved-pins');
  * TODO - export functions to be used for completing requests in ../routes/search.js
  */
 
+/**
+ * Function to GET saved pins from the SavedPins model
+ * @api {GET} /savedpins Request SavedPins information
+ * @apiSuccess 200 {SavedPins} returns a collection of SavedPins.
+ * @apiError 400 returns {error}
+ *
+ * @param {any} appReq
+ * @param {any} appRes
+ */
 exports.getSavedPins = (appReq, appRes) => {
   SavedPins.find().then((savedPins) => {
     appRes.send({ savedPins });
@@ -12,6 +21,15 @@ exports.getSavedPins = (appReq, appRes) => {
   });
 };
 
+/**
+ * Function to POST saved pins from the SavedPins model
+ * @api {POST} /savedpins saves {SavedPins}
+ * @apiSuccess 200 {SavedPins} returns the mongodb {SavedPins} after saved.
+ * @apiError 400 returns {error}
+ *
+ * @param {any} appReq
+ * @param {any} appRes
+ */
 exports.postSavedPins = (appReq, appRes) => {
   const savedPin = new SavedPins({
     lat: appReq.body.lat,
