ab633ff9e3294fc6b316dfb7ee9674d39ef930cf
John Kwening <kwening.john@gmail.com>
2018-02-02 04:31:06 -0500

    feat: Add user authentication (#71)
    Add jsonwebtokens and bcrypt packages as dependencies for the app. Add
    routes needed to authenticate and authorize user.
    
    Minor refactor to savedPins docstrings and status codes.

--- controllers/saved-pins-controller.js
+++ controllers/saved-pins-controller.js
@@ -1,34 +1,30 @@
 const SavedPins = require('../models/saved-pins');
 
 /**
- * TODO - export functions to be used for completing requests in ../routes/search.js
- */
-
-/**
- * Function to GET saved pins from the SavedPins model
- * @api {GET} /savedpins Request SavedPins information
- * @apiSuccess 200 {SavedPins} returns a collection of SavedPins.
- * @apiError 400 returns {error}
+ * @description Handles request for user saved pins
  *
- * @param {any} appReq
- * @param {any} appRes
+ * @api {GET} /savedpins
+ * @apiSuccess 200 {SavedPins} The collection of saved pins.
+ * @apiError 500 {server error} Problem finding all saved pins.
  */
 exports.getSavedPins = (appReq, appRes) => {
   SavedPins.find().then((savedPins) => {
     appRes.send({ savedPins });
   }, (e) => {
-    appRes.status(400).send(e);
+    appRes.status(500).send(e);
   });
 };
 
 /**
- * Function to POST saved pins from the SavedPins model
- * @api {POST} /savedpins saves {SavedPins}
- * @apiSuccess 200 {SavedPins} returns the mongodb {SavedPins} after saved.
- * @apiError 400 returns {error}
+ * @description Handles request to save a pin
  *
- * @param {any} appReq
- * @param {any} appRes
+ * @api {POST} /savedpins
+ * @apiSuccess 200 {SavedPins} The document representing the saved pin.
+ * @apiError 500 {server error} Problem saving the requested pin.
+ *
+ * @param {number} appReq.body.lat - lattitude coordinate for pin location
+ * @param {number} appReq.body.lng - longitude coordinate for pin location
+ * @param {string} appReq.body.place_id - name of pin location
  */
 exports.postSavedPins = (appReq, appRes) => {
   const savedPin = new SavedPins({
@@ -40,7 +36,6 @@ exports.postSavedPins = (appReq, appRes) => {
   savedPin.save().then((pin) => {
     appRes.send(pin);
   }, (e) => {
-    appRes.status(400).send(e);
+    appRes.status(500).send(e);
   });
 };
-
