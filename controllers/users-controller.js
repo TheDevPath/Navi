79ea4cf08600c608292a2c0ae581f6a3a7b3badc
John Kwening <kwening.john@gmail.com>
2018-02-04 22:40:33 -0500

    refactor: ESlint and code clean

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -17,35 +17,32 @@ const { JWT_KEY } = require('../config');
  * @param {string} appReq.body.password - user provided password
  */
 exports.registerUser = (appReq, appRes) => {
-  const validEmail;
-  const validPassword;
-   /**
+  /**
    *  Checks whether any of the fields are empty while submission.
    *  Checks whether the email address is of a valid format
-   *  Checks whether password is of minimum 6 characters & that it has atleast one number & atleast one specail character. 
+   *  Checks whether password is of minimum 6 characters & that it has atleast one number,
+   *   one letter, & atleast one specail character.
    */
-  validEmail = (email) => {
+  const validEmail = (email) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
-  } 
+  };
 
-  validPassword = (password) => {
+  const validPassword = (password) => {
     const re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/;
-    return re.test(String(password));   
-  }
-  
-  if(appReq.body.email && appReq.body.name && appReq.body.password){
-    
-    if(!validEmail(appReq.body.email)) return appRes.status(400).send('Email is not of the valid format');
-    if(!validPassword(appReq.body.password)) return appRes.status(400)
-        .send('Password should have minimum length of 6 & it should have atleast one special character, atleast one speacial character');
+    return re.test(String(password));
+  };
 
-  }
-  else{
-    return appRes.status(400).send('One or more fields were left empty')
+  if (appReq.body.email && appReq.body.name && appReq.body.password) {
+    if (!validEmail(appReq.body.email)) return appRes.status(400).send('Email is not of the valid format');
+    if (!validPassword(appReq.body.password)) {
+      return appRes.status(400)
+        .send('Password should have minimum length of 6 & it should have atleast one letter, one number, and one special character');
+    }
+  } else {
+    return appRes.status(400).send('One or more fields were left empty');
   }
 
-  
   // check whether user already exists with given email
   User.count({ email: appReq.body.email }, (err, result) => {
     if (err) {
@@ -54,8 +51,6 @@ exports.registerUser = (appReq, appRes) => {
     }
     if (result > 0) return appRes.status(409).send('Email already in use.');
   });
-
-
 
   // encrypt password
   const HASHED_PASSWORD = bcrypt.hashSync(appReq.body.password, 8);
