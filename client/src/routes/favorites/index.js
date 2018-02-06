c3c43fe88cb815229e5a10119b713223d1cb3dca
Emily Sperry <sperrye@gmail.com>
2018-02-05 23:36:13 -0800

    basic component

--- client/src/routes/favorites/index.js
+++ client/src/routes/favorites/index.js
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact';
+import style from './css/style.css';
 
 export default class Favorites extends Component {
   /* dummy data */
@@ -11,10 +12,11 @@ export default class Favorites extends Component {
     render({ }, { favorites }) {
         return (
           <div>
-            <h4>Favorites</h4>
+          Why
+            <h4 class={style.card__header}>Favorites</h4>
             <ul>
               { favorites.map((favorite, i) => (
-                  <li key={i}>
+                  <li key={i} class={style.card__item}>
                     {favorite.name}
                   </li>
               ))}
