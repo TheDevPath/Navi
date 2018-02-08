be955c51af47b48b1a163764e799a78517f2497d
motosharpley <motosharpley@gmail.com>
2018-02-07 22:15:27 -0600

    import map to home for testing

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -6,7 +6,6 @@ export default class Header extends Component {
 	render() {
 		return (
 			<header class={style.header}>
-				<h1>There and Back Again!</h1>
 				<nav>
 					<Link activeClassName={style.active} href="/">Home</Link>
 					<Link activeClassName={style.active} href="/profile">Me</Link>
--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -1,10 +1,12 @@
 import { h, Component } from 'preact';
 import style from './style';
+import MapContainer from '../../components/GoogleMap';
 
 export default class Home extends Component {
 	render() {
 		return (
 			<div class={style.home}>
+			<MapContainer />
 				<h1>Home</h1>
 				<p>This is the Home component.</p>
 			</div>
