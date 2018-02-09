4b1cee1a749824d747b96614577f37ae3cb14785
Raegan Millhollin <desdemonhu@gmail.com>
2018-02-09 09:29:43 -0800

    get user successful request complete

--- tests/users-test.js
+++ tests/users-test.js
@@ -1,7 +1,13 @@
 // api routes tests
 const request = require('supertest');
-
 const app = require('../app');
+
+const {
+  users, populateUsers,
+} = require('./seed/seed');
+
+
+beforeEach(populateUsers);
 
 describe('API Routes', () => {
   describe('GET /users/user', () => {
@@ -14,6 +20,19 @@ describe('API Routes', () => {
         expect(res).to.be.an('object');
       } catch (error) {
         throw error;
+      }
+    });
+    it('Sends the requested user when they exist', async () => {
+      try {
+        const res = await request(app)
+          .get('/users/user')
+          .set('x-access-token', users[0].tokens[0].token)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body._id).to.equal(`${users[0]._id}`);
+          });
+      } catch (err) {
+        throw err;
       }
     });
   });
