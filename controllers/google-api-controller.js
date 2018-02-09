9d689967a05cbb2e5fb906dbc52473eb42e5a045
John Kwening <kwening.john@gmail.com>
2018-02-08 22:06:26 -0500

    docs: Clean up docstrings

--- controllers/google-api-controller.js
+++ controllers/google-api-controller.js
@@ -21,8 +21,11 @@ const GEOLOCATION_OPTIONS = {
 
 /**
  * @description Hanlder for Geolocation API call
- * @returns {JSON} 'location': estimated 'lat' and 'lng'; 'accuracy': accuracy of
- * the estimated location, in meters
+ *
+ * @api {GET} /map/geolocation
+ * @apiSuccess 200 {JSON} 'location': estimated 'lat' and 'lng'; 'accuracy': accuracy of
+ *   the estimated location, in meters.
+ * @apiError 400 {request error} Google api request error.
  */
 exports.getGeolocation = (appReq, appRes) => {
   const req = http.request(GEOLOCATION_OPTIONS, (res) => {
@@ -34,8 +37,10 @@ exports.getGeolocation = (appReq, appRes) => {
 
     res.on('end', () => {
       const body = Buffer.concat(chunks);
-      appRes.send(JSON.parse(body.toString()));
+      appRes.status(200).send(JSON.parse(body.toString()));
     });
+  }).on('error', (err) => {
+    appRes.send(err);
   });
 
   req.write(JSON.stringify({}));
@@ -56,6 +61,10 @@ exports.getGeolocation = (appReq, appRes) => {
 /**
  * @description Handles request to get static map via google api
  *
+ * @api {POST} /map/staticmap
+ * @apiSuccess 200 {binary} Image file in the requested format.
+ * @apiError 400 {request error} Google api request error.
+ *
  * @param {number} appReq.body.centerLat - lattitude for map center
  * @param {number} appReq.body.centerLng - longitude for map center
  * @param {number} appReq.body.zoom - resolution of the current view [0 - 21+], default 15
@@ -67,8 +76,6 @@ exports.getGeolocation = (appReq, appRes) => {
  *   to center of the map (https://developers.google.com/maps/documentation/static-maps/intro#Markers)
  * @param {string} appReq.body.format - image type [png8, png32, gif, jpg, jpg-baseline],
  *   default is png8
- *
- * @returns {binary} Image file in the requested format
  */
 exports.getStaticMap = (appReq, appRes) => {
   const BASE_URL = 'https://maps.googleapis.com/maps/api/staticmap?';
@@ -114,6 +121,10 @@ exports.getStaticMap = (appReq, appRes) => {
 
 /**
  * @description Handles request for search autocompletion
+ *
+ * @api {POST} /search/autocomplete
+ * @apiSuccess 200 {JSON} Potential search queries.
+ * @apiError 400 {request error} Google api request error.
  *
  * @param {string} appReq.body.input - The text string on which to search.
  * @param {lat, lng} appReq.body.lat - (optional) The latitude for the point around which you
@@ -176,47 +187,51 @@ exports.autocomplete = (appReq, appRes) => {
  */
 
 /**
-  * @description Handles request for directions
-  *
-  * @param {string|lat,lng|place_id} origin - The address, textual latitude/longitude value,
-  *   or place ID from which you wish to calculate directions.
-  * @param {string|lat,lng|place_id} destination - The address, textual latitude/longitude value,
-  *   or place ID to which you wish to calculate directions.
-  * @param {string} mode - (optional) Specifies the mode of tranport to use for calculating
-  *   directions. [driving (default), walking, bicycling, transit]
-  *   (https://google-developers.appspot.com/maps/documentation/directions/intro#TravelModes)
-  * @param {...lat,lng|...place_id|...address} waypoints = (optional) Specifies a list of waypoints.
-  *   Waypoints alter a route by routing it through the specified location(s). A waypoint
-  *   is specified as a latitude/longitude coordinate, an encoded polyline, a place ID,
-  *   or an address which will be geocoded. Use pipe character delimitaton for more than one.
-  *   (https://google-developers.appspot.com/maps/documentation/directions/intro#Waypoints)
-  * @param {boolean} alternatives - (optional) If set to true, specifies that the Directions
-  *   service may provide more than one route alternative in the response. Note that providing
-  *   route alternatives may increase the response time from the server.
-  * @param {...string} avoid - (optional) Indicates that the calculated route(s) should
-  *   avoid the indicated features. [tolls, highways, ferries, indoor]
-  *   Multiple value example:  avoid=tolls|highways|ferries.
-  * @param {number} arrival_time - (optional) Specifies the desired time of arrival for
-  *   transit directions, in seconds since midnight, January 1, 1970 UTC.
-  * @param {number} departure_time - (optional) Specifies the desired time of departure.
-  *   You can specify the time as an integer in seconds since midnight, January 1, 1970 UTC.
-  *   Alternatively, you can specify a value of now, which sets the departure time to the
-  *   current time (correct to the nearest second). The departure time may be specified in
-  *   two cases: 1. travel mode is transit 2. travel mode is driving.
-  * @param {string} traffic_model - (defaults to best_guess) Specifies the assumptions to use
-  *   when calculating time in traffic. The traffic_model parameter may only be specified for
-  *   driving directions where the request includes a departure_time.
-  *   [best_guess, pessimistic, optimistic]
-  * @param {string} transit_mode - (optional) Specifies one or more preferred modes of transit.
-  *   This parameter may only be specified for transit directions. [bus, subway, train, tram, rail]
-  *   rail - equivalent to transit_mode = train|tram|subway
-  * @param {string} transit_routing_preference - (optional) Specifies preferences for transit
-  * routes. Using this parameter, you can bias the options returned, rather than accepting
-  * the default best route chosen by the API. [less_walking, fewer_transfers]
-  *
-  * @returns {Object} Default google maps json response
-  *   (https://google-developers.appspot.com/maps/documentation/directions/intro#DirectionsResponseElements)
-  */
+ * @description Handles request for directions
+ *
+ * @api {POST} /map/directions
+ * @apiSuccess 200 {JSON} Directions result.
+ * @apiError 400 {request error} Google api request error.
+ *
+ * @param {string|lat,lng|place_id} origin - The address, textual latitude/longitude value,
+ *   or place ID from which you wish to calculate directions.
+ * @param {string|lat,lng|place_id} destination - The address, textual latitude/longitude value,
+ *   or place ID to which you wish to calculate directions.
+ * @param {string} mode - (optional) Specifies the mode of tranport to use for calculating
+ *   directions. [driving (default), walking, bicycling, transit]
+ *   (https://google-developers.appspot.com/maps/documentation/directions/intro#TravelModes)
+ * @param {...lat,lng|...place_id|...address} waypoints = (optional) Specifies a list of waypoints.
+ *   Waypoints alter a route by routing it through the specified location(s). A waypoint
+ *   is specified as a latitude/longitude coordinate, an encoded polyline, a place ID,
+ *   or an address which will be geocoded. Use pipe character delimitaton for more than one.
+ *   (https://google-developers.appspot.com/maps/documentation/directions/intro#Waypoints)
+ * @param {boolean} alternatives - (optional) If set to true, specifies that the Directions
+ *   service may provide more than one route alternative in the response. Note that providing
+ *   route alternatives may increase the response time from the server.
+ * @param {...string} avoid - (optional) Indicates that the calculated route(s) should
+ *   avoid the indicated features. [tolls, highways, ferries, indoor]
+ *   Multiple value example:  avoid=tolls|highways|ferries.
+ * @param {number} arrival_time - (optional) Specifies the desired time of arrival for
+ *   transit directions, in seconds since midnight, January 1, 1970 UTC.
+ * @param {number} departure_time - (optional) Specifies the desired time of departure.
+ *   You can specify the time as an integer in seconds since midnight, January 1, 1970 UTC.
+ *   Alternatively, you can specify a value of now, which sets the departure time to the
+ *   current time (correct to the nearest second). The departure time may be specified in
+ *   two cases: 1. travel mode is transit 2. travel mode is driving.
+ * @param {string} traffic_model - (defaults to best_guess) Specifies the assumptions to use
+ *   when calculating time in traffic. The traffic_model parameter may only be specified for
+ *   driving directions where the request includes a departure_time.
+ *   [best_guess, pessimistic, optimistic]
+ * @param {string} transit_mode - (optional) Specifies one or more preferred modes of transit.
+ *   This parameter may only be specified for transit directions. [bus, subway, train, tram, rail]
+ *   rail - equivalent to transit_mode = train|tram|subway
+ * @param {string} transit_routing_preference - (optional) Specifies preferences for transit
+ * routes. Using this parameter, you can bias the options returned, rather than accepting
+ * the default best route chosen by the API. [less_walking, fewer_transfers]
+ *
+ * @returns {Object} Default google maps json response
+ *   (https://google-developers.appspot.com/maps/documentation/directions/intro#DirectionsResponseElements)
+ */
 exports.directions = (appReq, appRes) => {
   // handle optional params that will need additonal formating
   const alternatives = (appReq.body.alternatives) ? true : '';
