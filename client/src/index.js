e392662f931ceb4caf2b7e3c61bad33f39a7aed7
Kwening <jkwening@air.org>
2018-02-14 13:49:27 -0500

    feat: Add service worker for caching

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,19 @@
 import './style';
 import App from './components/app';
 
+/**
+ * Register service worker anytime the main app component runs
+ */
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+
 export default App;
