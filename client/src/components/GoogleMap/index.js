bebfb79d79e464c7db53267568b296ca6ebd81aa
John Kwening <kwening.john@gmail.com>
2018-02-20 15:01:17 -0500

    feat: Show default route in directions component

--- client/src/components/GoogleMap/index.js
+++ client/src/components/GoogleMap/index.js
@@ -1,78 +1,24 @@
 import { h, Component } from 'preact';
-const { GOOGLE_API_KEY } = require('../../../config');
 
-import Map, { GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
-
-class MapContainer extends Component {
+export default class MapContainer extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            showingInfoWindow: false,
-            activeMarker: {},
-            selectedPlace: {},
-            lat: null,
-            lng: null,
         }
-        this.onMarkerClick = this.onMarkerClick.bind(this);
-        this.onMapClicked = this.onMapClicked.bind(this);
-        this.geolocatonSuccess = this.geolocatonSuccess.bind(this);
-        navigator.geolocation.getCurrentPosition(this.geolocatonSuccess);
-    }
-
-    geolocatonSuccess(position) {
-        this.setState({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-        });
-    }
-
-    onMarkerClick = (props, marker, e) => {
-        this.setState({
-            selectedPlace: props,
-            activeMarker: marker,
-            showingInfoWindow: true
-        });
-    };
-
-    onMapClicked = (props) => {
-        if (this.state.showingInfoWindow) {
-            this.setState({
-                showingInfoWindow: false,
-                activeMarker: null
-            })
-        }
-    };
 
     render() {
+        const styles = {
+            height: this.props.height,  // HAVE TO SET HEIGHT TO RENDER MAP
+        }
         return (
-            <Map
-                google={this.props.google}
-                onClick={this.onMapClicked}
-                zoom={13}
-                center={{
-                    lat: this.state.lat,
-                    lng: this.state.lng,
-                }}
-            >
-                <Marker
-                    onClick={this.onMarkerClick}
-                    name={'Current location'}
-                    position={{lat: this.state.lat, lng: this.state.lng}}
-                />
-
-                <InfoWindow
-                    marker={this.state.activeMarker}
-                    visible={this.state.showingInfoWindow}
-                >
-                    <h1>
-                        {this.state.selectedPlace.name}
-                    </h1>
-                </InfoWindow>
-            </Map>
+            <div id="map" style={styles}/>
         )
     };
-}
 
-export default GoogleApiWrapper({
-    apiKey: GOOGLE_API_KEY,
-})(MapContainer)
+    componentWillUnmount() {
+        // console.log('begin - componentWillUnmount() ', this.state.map);
+        // // stop watching user position when map is unmounted
+        // navigator.geolocation.clearWatch(this.state.watchID);
+        // this.state.map.remove();
+        // console.log('begin - componentWillUnmount() ', this.state.map);
+    }
+}
