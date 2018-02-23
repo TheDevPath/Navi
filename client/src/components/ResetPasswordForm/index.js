e247f8917aa675fe1f84f683d7b313f1aead0b79
Stephen Chavez <stephen@dicesoft.net>
2018-02-23 14:06:05 -0700

    reset and forgot password page refactored

--- client/src/components/ResetPasswordForm/index.js
+++ client/src/components/ResetPasswordForm/index.js
@@ -1,49 +1,26 @@
-import { h, Component } from 'preact';
-import { Link } from 'preact-router/match';
+import {h, Component} from 'preact';
+import {Link} from 'preact-router/match';
 import PropTypes from 'prop-types';
 import style from './style';
 
-import Button from 'preact-material-components/Button';
-import 'preact-material-components/Button/style.css';
-import 'preact-material-components/Theme/style.css';
-
-import Elevation from 'preact-material-components/Elevation';
-import 'preact-material-components/Elevation/style.css';
-
-import TextField from 'preact-material-components/TextField';
-import 'preact-material-components/TextField/style.css';
-
 export default class ResetPasswordForm extends Component {
-	render() {
-		return (
-			<Elevation z={2}>
-				<div class={style['reset-password-form']}>
-					<div class='mdc-form-field mdc-form-field--align-end'>
-						<h1>We sent you a temporary password. Please change it below:</h1>
-						<form action="/">
-							<div>
-								<TextField label="Email" name="email"/>
-							</div>
-							<div>
-								<TextField label="Temporary Password" name="password" type="password"/>
-							</div>
-							<div>
-								<TextField label="New Password" name="new_password" type="password"/>
-							</div>
-							<div>
-								<TextField label="Confirm New Password" name="confirm_password" type="password"/>
-							</div>
-							<Button unelevated className="mdc-theme--primary-bg">
-								Join
-							</Button>
-						</form>
-					</div>
-				</div>
-			</Elevation>
-		);
-	}
+    render() {
+        return (<div class={style['reset-password-form']}>
+            <h1>We sent you a temporary password. Please change it below.</h1>
+            <form action="/">
+                <span>Email: </span><input type="text" name="email"/><br/>
+                <span>New Password: </span><input type="text" name="password"/><br/>
+                <span>Confirm Password: </span><input type="text" name="confirm_password"/><br/>
+
+                <input type="submit" value="Reset Account Password"/>
+            </form>
+            <a class={style.active} href="/register">register</a>
+            <br/>
+            <a class={style.active} href="/signin">sign in</a>
+        </div>);
+    }
 }
 
 ResetPasswordForm.propTypes = {
-	appName: PropTypes.string,
+    appName: PropTypes.string
 }
