c3b00b0c95ae7e270151eda45b20b2f51b88633a
John Kwening <KweningJ@Johns-MacBook-Pro.local>
2018-02-26 00:20:56 -0500

    refactor: Overhaul map view into MaxExplorer
    Add feature for user to drop markers with option to drag, remove, and
    save dropped markers.
    
    Add circle marker for user's current position.
    
    Add nicer style for search menu that looks embedded within MapPane.
    
    Remove redundant components

--- client/src/routes/maps/index.js
+++ client/src/routes/maps/index.js
@@ -1,18 +1,17 @@
 import { h, Component } from "preact";
 import style from "./style";
-// import MapContainer from '../../components/LeafletOsmMap';
-import Search from '../../components/AppSearch'
+import MapContainer from '../../components/LeafletOsmMap';
+import Search from '../../components/Search'
 import SearchResults from '../../components/SearchResults';
-import Explore from '../../components/Explore';
 
-export default class Maps extends Component {
+export default class MapExplorer extends Component {
   render() {
     return (
       <div class={style.maps}>
         <Search>
           <SearchResults />
         </Search>
-        <Explore />
+        <MapContainer />
       </div>
     );
   }
