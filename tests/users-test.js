78828e388f10704a7c2407684cdf7f77c8c25db5
Raegan Millhollin <desdemonhu@gmail.com>
2018-02-27 10:48:52 -0800

    Added tests for users/login and users/logout

--- tests/users-test.js
+++ tests/users-test.js
@@ -11,7 +11,7 @@ beforeEach(populateUsers);
 afterEach(deleteTestUser);
 afterEach(stopServer);
 
-describe('API Routes', () => {
+describe('/Users API Routes', () => {
   describe('GET /users/user', () => {
     it('does not send a user if not logged in', (done) => {
       try {
@@ -40,7 +40,7 @@ describe('API Routes', () => {
   });
   // /register- POST
   // valid email, password, and doesn't exist already
-  
+
   describe('POST users/register', () => {
     it('succesfully creates a new user', (done) => {
       try {
@@ -62,8 +62,8 @@ describe('API Routes', () => {
       }
     });
 
-// valid email and password, but exists already
-    it('User already exists', (done) => {
+    // valid email and password, but exists already
+    it('Wont create a new user if email address already exists', (done) => {
       try {
         request(app)
           .post('/users/register')
@@ -81,7 +81,7 @@ describe('API Routes', () => {
         done(err);
       }
     });
-  // invalid email and try to create
+    // invalid email and try to create
     it('Wont create user with invalid email', (done) => {
       try {
         request(app)
@@ -101,7 +101,7 @@ describe('API Routes', () => {
         done(err);
       }
     });
-  // invalid password and try to create
+    // invalid password and try to create
     it('Wont create user with invalid password', (done) => {
       try {
         request(app)
@@ -127,10 +127,79 @@ describe('API Routes', () => {
 
   // /login - POST
   // apiSuccess 200 { auth: true, token: token } jsonwebtoken.
-  // apiError 400 { request error } User not found.
-  // apiError 401 { auth: false, token: null } Invalid password.
-  // apiError 500 { server error } Problem finding user.
+  describe('POST /users/login', () => {
+    it('Successfully logs the user in', (done) => {
+      try {
+        request(app)
+          .post('/users/login')
+          .send({
+            email: users[0].email,
+            password: users[0].password,
+          })
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).to.equal({ auth: true, token: users[0].tokens.token });
+          });
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+    // apiError 400 { request error } User not found.
+    it('Should send 404 if user is not found', (done) => {
+      try {
+        request(app)
+          .post('/users/login')
+          .send({
+            email: 'gibberish@gmail.com',
+            password: users[0].password,
+          })
+          .expect(404)
+          .expect((res) => {
+            expect(res.body).to.equal('No user found.');
+          });
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+    // apiError 401 { auth: false, token: null } Invalid password.
+    it('Should send 401 if user has invalid password', (done) => {
+      try {
+        request(app)
+          .post('/users/login')
+          .send({
+            email: users[0].email,
+            password: 'badpassword',
+          })
+          .expect(401)
+          .expect((res) => {
+            expect(res.body).to.equal({ auth: false, token: null });
+          });
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+    // apiError 500 { server error } Problem finding user.
+  });
 
   // /logout - GET
-  //  apiSuccess 200 { auth: false, token: null }
+  describe('GET /users/logout', () => {
+    //  apiSuccess 200 { auth: false, token: null }
+    it('Should send 200 if logout is successful', (done) => {
+      try {
+        request(app)
+          .get('/users/logout')
+          .set('x-access-token', users[0].tokens[0].token)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body).to.equal({ auth: false, token: null });
+          });
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
 });
