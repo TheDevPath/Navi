9a864c4df521d7d010884cb3e018888a1e81c44d
Siobhan Williams <vonnie2k5@gmail.com>
2018-03-06 11:55:38 -0500

    feat: Add component for reloading saved pins
    Created separate component for preloading saved pins with additional
    functions that can be used independently for future features (such as
    viewing pins from another user)
    
    Issue: #183

---
+++ client/src/components/LeafletOsmMap/SavedPlaces.js
@@ -0,0 +1,128 @@
+import {h, Component} from "preact";
+import {makeRequest} from "../../js/server-requests-utils";
+
+/**
+ * Leaflet related imports: leaflet, pouchdb module, and routing machine module
+ */
+import '../../../node_modules/leaflet/dist/leaflet.css';
+import '../../../node_modules/leaflet-routing-machine/dist/leaflet-routing-machine.css';
+import L from '../../js/leaflet-tileLayer-pouchdb-cached';
+import Routing from '../../../node_modules/leaflet-routing-machine/src/index.js';
+
+/* Set all settings for saved place icon
+Full list of options: http://leafletjs.com/reference-1.3.0.html#icon
+ */
+const FAV_MARKER_ICON = L.icon({
+  iconUrl: '../../icons/marker-icon-fav.png',
+  shadowUrl: '../../icons/marker-shadow.png',
+  className: 'favorites',
+});
+
+const CURRENT_USER = makeRequest('GET', 'user');
+
+
+
+export default class SavedPlaces extends Component {
+  constructor(props) {
+    super(props)
+    const {user, map} = props;
+
+    fetchAndDropUserPins(user, map);
+  }
+
+  render() {
+    //This shouldn't render much (if anything)
+    return ( null  );
+  }
+
+}
+
+
+/**
+ * Exported Functions
+ */
+
+//Get and drop pins from any user on any map
+const fetchAndDropUserPins =  (userPromised=CURRENT_USER, mapObj=undefined) => {
+
+  userPromised
+  .then(res => res.data._id) //get user ID
+  .then(user_id => getSavedPins(user_id))
+  .then(savedPins => {
+
+    let pinMarkers = makePinMarkers(savedPins);
+    if (mapObj != null) dropPin(pinMarkers, mapObj);
+
+  })
+  .catch(err => {
+    //User not signed in, no saved places to load
+    console.log(`Error. abort load saved places: ${err}`);
+  });
+  
+}
+
+const getSavedPins = (user_id='') => {
+  //GET 'search/savedpins/:user_id'
+  let pinsPromised = makeRequest('GET', 'savedPins', '', user_id); 
+ 
+  return pinsPromised
+  .then(res => res.data.savedPins)
+  .catch(err => {
+    // console.log(`Couldn't get the user pins: ${err}`);
+
+    //rethrow error
+    throw(err);
+  });
+
+}
+
+//This function generates markers and drops them on a map (if specified)
+const makePinMarkers = (pinArray = []) => {
+ 
+  let pinMarkers = [];
+  
+
+  for (let pin of pinArray) {
+    //create  marker for the pin and bind it ot the map
+    let thisMarker =  L.marker([pin.lat, pin.lng], {
+      icon: FAV_MARKER_ICON,
+      draggable: false,
+      autopan: true,
+      riseOnHover: true,
+      title: pin.place_id
+    });
+
+    pinMarkers.push(thisMarker);
+  }
+
+  // console.log('User Pins added: ',pinMarkers)
+  return pinMarkers;
+
+}
+
+//This funciton drops pins on a map replacing pins if they already exist
+const dropPin = (pinMarkers=[], mapObj=undefined) => {
+  
+  //if no map is defined, or there are no pins return
+   if ((mapObj == null) || (pinMarkers.length == 0)) return;
+
+  /*TO DO?:
+  Do not allow duplication of pins,
+  Remove those that exist already on the old map
+  Hint: use sets
+  */
+
+
+  for (let marker of pinMarkers){
+    marker.addTo(mapObj);
+
+    /* TO DO
+    Bind a custom container for each pin on click */
+    marker.bindPopup(marker.title);
+  }
+}
+
+
+//Export Statements
+export {fetchAndDropUserPins, getSavedPins, makePinMarkers, dropPin};
+
