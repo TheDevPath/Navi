7784299b4bfb6ba22e80a142f450072937e5ad41
seckboy <e_ford.geo@yahoo.com>
2018-03-07 15:33:33 -0600

    add saved pins and searches to profile

--- client/config/dev.config.js
+++ client/config/dev.config.js
@@ -4,4 +4,6 @@ module.exports = {
     REGISTER_PATH: '/users/register',
     LOGIN_PATH: '/users/login',
     RESET_PATH: '/users/reset-password',
+    SEARCH_HISTORY_PATH: '/search/history',
+    SAVED_PINS_PATH: '/search/savedpins',
 };
