fd92f849045ee0616e7d2feca4e0e9ff13a76e54
John Kwening <kwening.john@gmail.com>
2018-03-09 20:38:38 -0500

    feat: Handle Navbar routing

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,16 +1,30 @@
 import {h, Component} from 'preact';
+import { route } from 'preact-router';
 import style from './style.css';
 
 export default class Nav extends Component {
+  constructor(props) {
+    super(props);
 
-  componentDidMount() {
-  }
+    this.routeToHome = this.routeToHome.bind(this);
+    this.routeToProfile = this.routeToProfile.bind(this);
+  } 
+
+  routeToHome() {
+    route('/', true);
+  };
+
+  routeToProfile() {
+    route('/profile', true);
+  };
 
   render() {
     return (
       <div class={style.nav}>
-        <img src='../../assets/icons/leaflet/SVG/NaviHomeIcon.svg' alt='Home screen icon' class={style.homeIcon}/>
-        <img src='../../assets/icons/leaflet/SVG/ProfileIcon.svg' alt='Profile page icon' class={style.profileIcon}/>
+        <img src='../../assets/icons/leaflet/SVG/NaviHomeIcon.svg' alt='Home screen icon'
+          class={style.homeIcon} onClick={this.routeToHome}/>
+        <img src='../../assets/icons/leaflet/SVG/ProfileIcon.svg' alt='Profile page icon'
+          class={style.profileIcon} onClick={this.routeToProfile}/>
       </div>
     );
   }
