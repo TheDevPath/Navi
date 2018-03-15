5fcc912554e1c005cd12867a55bb1351ebb08c2f
Alice Palazzolo <35308564+alicepalazzolo@users.noreply.github.com>
2018-03-14 21:44:11 -0400

    Homepage Styling
    Background Image
    
    Responsive Logo
    
    Responsive Input Field
    
    Style my Location
    
    Style Input Text Box
    
    Update Font

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -28,10 +28,11 @@ export default class Home extends Component {
 						<SearchResults />
 					</Search>
 				</div>
-				<div class="myLocation"><a href="/maps" class={style.mapLink}>where am I?</a>
-				<img src='../../assets/icons/leaflet/SVG/CurrentLocationPin.svg'
+				<div class={style.myLocation}>
+				<a href="/maps" class={style.mapLink}>where am I?</a>
+				<img src='../../assets/icons/leaflet/SVG/FindLocationPin.svg'
 					class={style.pin} onClick={this.routeToMap}/>	
-					</div>
+				</div>
 			</div>
 		);
 	}
