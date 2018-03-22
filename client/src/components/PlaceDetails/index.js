9b06827beee254d462fcd6f7520c40e5098b162e
turisap <kirill-shakirov@mail.ru>
2018-03-22 17:20:55 +0700

    PlaceDetails component. Obtaining place details from backend through ComponentDidMount() method

--- client/src/components/PlaceDetails/index.js
+++ client/src/components/PlaceDetails/index.js
@@ -3,10 +3,11 @@
  */
 import { h, Component } from "preact";
 import style from "./style";
-import {makeRequest} from "../../js/server-requests-utils";
+import {makeRequest, BASE_ENDPOINTS} from "../../js/server-requests-utils";
 import axios from 'axios';
 import classNames from 'classnames';
 import 'font-awesome/css/font-awesome.min.css';
+import Cookies from "universal-cookie";
 
 const CONFIG = require('../../../../config/secrets.json');
 
@@ -19,10 +20,10 @@ export default class PlaceDetails extends Component {
         // for making a request I keep two properties for state: placeId for making a request
         // and a place object itself
         this.placeId = 'ChIJxxRh5LCe4jARFw0N68oQ9cw';
-        this.place = props.place || testPlace;
         this.state = {
             fullScreenMode : true,
             showWorkingHours : false,
+            place : null
         }
     }
 
@@ -44,9 +45,10 @@ export default class PlaceDetails extends Component {
      * @returns {XML[]}
      */
     partialScreenMode () {
+        const {formatted_address, name} = this.state.place || testPlace;
         return [
-            <div class={style.details__item}>{this.place.placeName}</div>,
-            <div class={style.details__item}>{this.place.address}</div>,
+            <div class={style.details__item}>{name}</div>,
+            <div class={style.details__item}>{formatted_address}</div>,
             <div class={style.details__item}><a href="#" onClick={e => this.setState({fullScreenMode : true})}>More about this place</a></div>
         ];
     }
@@ -57,41 +59,56 @@ export default class PlaceDetails extends Component {
      * @returns {XML[]}
      */
     fullScreenMode () {
+        const {
+            name,
+            formatted_address,
+            formatted_phone_number,
+            website,
+            opening_hours,
+            imgURL
+            } = this.state.place;
         return [
             <div class={style.details__item}>
-                <img alt={`${this.place.name} picture`} src={this.place.imgURL} class={style.place__image}/>
+                <img alt={`${name} picture`} src={imgURL} class={style.place__image}/>
             </div>,
-            <div class={style.details__item}>{this.place.name}</div>,
-            <div class={style.details__item}>{this.place.address}</div>,
-            <div class={style.details__item}><img src={this.place.pictureURL}/></div>,
-            <div class={style.details__item}>{this.place.phoneNumber}</div>,
-            <div class={style.details__item}>{this.place.website}</div>,
+            <div class={style.details__item}>{name}</div>,
+            <div class={style.details__item}>{formatted_address}</div>,
+            <div class={style.details__item}>{formatted_phone_number}</div>,
+            <div class={style.details__item}>{PlaceDetails.prettyWebsiteName(website)}</div>,
             <div class={style.details__item}>
                 <div>
-                    Open now: {this.place.openNow ? 'yes' : 'no'}
+                    Open now: {opening_hours.open_now ? 'yes' : 'no'}
                     <span class="style.details__days-toggler"
                           onClick={() => this.setState({showWorkingHours: !this.state.showWorkingHours})}>
                         <i class="fa fa-angle-down"></i>
                     </span>
                 </div>
                 <div class={this.state.showWorkingHours ? style.hours__seen : style.hours__hidden}>
-                    {this.place.openingHours.map(period => <div>{period}</div>)}
+                    {opening_hours.weekday_text.map(period => <div>{period}</div>)}
                 </div>
             </div>,
             <div class={style.details__item}><a href="#" onClick={e => this.setState({fullScreenMode: false})}>X</a></div>
         ]
     }
 
+
+    /**
+     * Getting details of a place based on it's id
+     * Sets state.place to response object from back-end
+     */
     componentDidMount() {
-        console.log('===========================================================');
-        makeRequest('GET', 'places', '', '', {place_id : this.placeId})
-            .then((data) =>  console.log('DATA', data))
-            .catch(e => console.error('ERROR', e))
+        makeRequest('GET', BASE_ENDPOINTS.places, `/${this.placeId}`)
+            .then(data =>  {
+                console.log(data.data);
+                if(!data.data.error_message) this.setState({place: data.data.result})
+            })
+            .catch(e => console.error('ERROR1', e))
     }
 
 
     render () {
         return (
+            this.state.place &&
             <div class={style.placeDetails__container}>
                 <div class={this.state.fullScreenMode ? style.placeDetails__innerbig : style.placeDetails__innersmall}>
                     <button class={style.directionsButton}>Directions</button>
@@ -101,6 +118,18 @@ export default class PlaceDetails extends Component {
         )
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /**
