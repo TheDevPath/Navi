902b2296085a08c6b169619dec9315030e261db1
someyoungideas <someyoungideas@gmail.com>
2018-03-30 22:45:58 -0500

    updating home route indentation

--- client/src/routes/home/index.js
+++ client/src/routes/home/index.js
@@ -5,86 +5,86 @@ import Search from '../../components/Search';
 import SearchResults from '../../components/SearchResults';
 
 export default class Home extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			busyMessage: 'Loading your location...',
-			userPosition: {
-				lat: '',
-				lng: ''
-			},
-		}
+    this.state = {
+      busyMessage: 'Loading your location...',
+      userPosition: {
+        lat: '',
+        lng: ''
+      },
+    }
 
-		this.routeToMap = this.routeToMap.bind(this);
-	}
+    this.routeToMap = this.routeToMap.bind(this);
+  }
 
-	componentDidMount() {
-		if (this.state.userPosition.lat && this.state.userPosition.lng) {
-			this._setBusyMessage('');
-			return;
-		}		
+  componentDidMount() {
+    if (this.state.userPosition.lat && this.state.userPosition.lng) {
+      this._setBusyMessage('');
+      return;
+    }   
 
-		if ('geolocation' in navigator === false){
-			this._setBusyMessage('Could not find your location');
-			return;
-		}
+    if ('geolocation' in navigator === false){
+      this._setBusyMessage('Could not find your location');
+      return;
+    }
 
-		const self = this;
-		navigator.geolocation.getCurrentPosition(
-			(position) => {
-				this.setState({
-					busyMessage: '',
-					userPosition: {
-						lat: position.coords.latitude,
-						lng: position.coords.longitude
-					}
-				});
-				console.log('\tuser position found: ', this.state.userPosition);
-			},
-			(err) => {
-				if (err.code && err.code === 1)
-					self._setBusyMessage('Could not find your location');
-				else
-					self._setBusyMessage('Error occurred while getting your location');
-			},
-			{
-				enableHighAccuracy: false,
-				timeout: 60000,
-				maximumAge: Infinity,
-			}
-		);
-	}
+    const self = this;
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          busyMessage: '',
+          userPosition: {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          }
+        });
+        console.log('\tuser position found: ', this.state.userPosition);
+      },
+      (err) => {
+        if (err.code && err.code === 1)
+          self._setBusyMessage('Could not find your location');
+        else
+          self._setBusyMessage('Error occurred while getting your location');
+      },
+      {
+        enableHighAccuracy: false,
+        timeout: 60000,
+        maximumAge: Infinity,
+      }
+    );
+  }
 
-	routeToMap() {
-		route('/maps', true);
-	}
+  routeToMap() {
+    route('/maps', true);
+  }
 
-	_setBusyMessage(message) {
-		this.setState({ busyMessage: message });
-	}
+  _setBusyMessage(message) {
+    this.setState({ busyMessage: message });
+  }
 
-	render() {
-		return (
-			<div class={style.home}>
-				<div class={style.welcome}>
-					<h2>WELCOME TO</h2>
-					<img src='../../assets/icons/leaflet/SVG/darkLogo.svg' />
-				</div>
-				<div class={style.search}>
-					<p>Where can we take you today?</p>
-					<Search position={this.state.userPosition} routeUrl={this.props.url}
-						updateSearchResult={this.props.updateSearchResult}>
-						<SearchResults />
-					</Search>
-					<p>{this.state.busyMessage || ''}</p>
-				</div>
-				<div class={style.myLocation}>
-				<a href="/maps" class={style.mapLink}>where am I?</a>
-				<img src='../../assets/icons/leaflet/SVG/FindLocationPin.svg'
-					class={style.pin} onClick={this.routeToMap}/>	
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div class={style.home}>
+        <div class={style.welcome}>
+          <h2>WELCOME TO</h2>
+          <img src='../../assets/icons/leaflet/SVG/darkLogo.svg' />
+        </div>
+        <div class={style.search}>
+          <p>Where can we take you today?</p>
+          <Search position={this.state.userPosition} routeUrl={this.props.url}
+            updateSearchResult={this.props.updateSearchResult}>
+            <SearchResults />
+          </Search>
+          <p>{this.state.busyMessage || ''}</p>
+        </div>
+        <div class={style.myLocation}>
+        <a href="/maps" class={style.mapLink}>where am I?</a>
+        <img src='../../assets/icons/leaflet/SVG/FindLocationPin.svg'
+          class={style.pin} onClick={this.routeToMap}/> 
+        </div>
+      </div>
+    );
+  }
 }
